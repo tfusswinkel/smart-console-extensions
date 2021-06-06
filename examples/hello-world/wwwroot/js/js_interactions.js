2d1620af01f051d4aa319ea45827deff25105684
Torsten Fußwinkel <51260483+tfusswinkel@users.noreply.github.com>
2021-06-06 10:52:31 +0200

    Migrate project hello-world to BlazorWebAssembly

--- examples/hello-world/wwwroot/js/js_interactions.js
+++ examples/hello-world/wwwroot/js/js_interactions.js
@@ -1,9 +1,19 @@
-﻿$(document).ready(function () {
-    var params = { "callback": "onContext" };
-    smxProxy.sendRequest("get-context", null, "onContext");
-});
+﻿var smxInterop = function () {
+    return {
+        globalCallbackFunctionName: null,
+        smxInvoker: function (smxInteraction, smxParameter, smxCallbackFunctionName, dotNetCallbackReference) {
+            this.globalCallbackFunctionName = smxCallbackFunctionName;
 
-function onContext(context) {
-    var tree = JSONTree.create(context);
-    $("#example").html(tree);
+            window[this.globalCallbackFunctionName] = (result) => {
+                dotNetCallbackReference.invokeMethod("SmxResultCallback", result);
+            };
+
+            smxProxy.sendRequest(smxInteraction, smxParameter, this.globalCallbackFunctionName);
+        },
+        smxDispose: function () {
+            if (window.hasOwnProperty(this.globalCallbackFunctionName)) {
+                delete window[this.globalCallbackFunctionName];
+            }
+        }
+    }
 }
